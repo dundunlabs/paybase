90e21a3b42410996cd7af17aa09b8798ee312fb8
Dung Tran <dung28.td@gmail.com>
2022-11-06 11:34:11 +0700

    Initialize project

---
+++ rest/src/main/java/dev/paybase/rest/controller/HomeController.java
@@ -0,0 +1,12 @@
+package dev.paybase.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+}
